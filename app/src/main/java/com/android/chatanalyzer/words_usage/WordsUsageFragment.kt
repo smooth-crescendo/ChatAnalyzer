32aaffff054e6b11d45107361ae9f5ee91db6832
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2021-02-02 21:51:42 +0300

    Minor code changes

--- app/src/main/java/com/android/chatanalyzer/words_usage/WordsUsageFragment.kt
+++ app/src/main/java/com/android/chatanalyzer/words_usage/WordsUsageFragment.kt
@@ -1,14 +1,11 @@
 package com.android.chatanalyzer.words_usage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.android.chatanalyzer.R
-import com.android.chatanalyzer.databinding.FragmentChatStatsBinding
 import com.android.chatanalyzer.databinding.FragmentWordsUsageBinding
 
 class WordsUsageFragment : Fragment() {
